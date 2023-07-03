037a95ecc98d0ae4cdae601fc7ed8f28be413d74
ahaddad98 <amine.haddadd@gmail.com>
2023-07-03 13:03:23 +0100

    add gmail sender

--- app/contactme/mailpart/page.tsx
+++ app/contactme/mailpart/page.tsx
@@ -1,11 +1,23 @@
-import React from "react";
-'use-client'
+'use client'
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import CardSnippet from "./Card";
 // import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 // import { vs } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 
 const BioPage = () => {
+    const formRef = useRef<any>();
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        console.log(e);
+        emailjs.sendForm('service_8zqmdvo', 'template_jtpb7dq', formRef.current, 'vHn-08m5ljeQ21YXL')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
     return <React.Fragment>
         <div className="h-full flex-1 flex flex-col">
             <div className="hidden h-[42px] w-[200px] xl:flex items-center justify-between pl-3 pr-3 border-r-2 border-r-bordercolor">
@@ -13,21 +25,21 @@ const BioPage = () => {
                 <a>x</a>
             </div>
             <div className="w-full divider divider-vertical m-0 mt-0 p-0 h-1"></div>
-            <form className="flex flex-col flex-1 xl:overflow-y-scroll p-5 text-[17px] text-mygray max-h-[660px] items-center justify-center gap-4">
+            <form ref={formRef} onSubmit={(e: any) => { sendEmail(e) }} className="flex flex-col flex-1 xl:overflow-y-scroll p-5 text-[17px] text-mygray max-h-[660px] items-center justify-center gap-4">
                 <div className="flex flex-col gap-1 w-full max-w-xs">
                     <span className="label-text">_name:</span>
-                    <input type="text" placeholder="Type here" className="input border-2 border-bordercolor bg-cardbg  w-full max-w-xs" />
+                    <input type="text" name="user_name" placeholder="Type here" className="input border-2 border-bordercolor bg-cardbg  w-full max-w-xs" />
                 </div>
                 <div className="flex flex-col gap-1 w-full max-w-xs">
                     <span className="label-text">_email:</span>
-                    <input type="text" placeholder="Type here" className="input border-2 border-bordercolor bg-cardbg  w-full max-w-xs" />
+                    <input name="user_email" type="text" placeholder="Type here" className="input border-2 border-bordercolor bg-cardbg  w-full max-w-xs" />
                 </div>
                 <div className="flex flex-col gap-1 w-full max-w-xs">
                     <span className="label-text">_message:</span>
-                    <textarea className="textarea textarea-ghost border-2 border-bordercolor bg-cardbg max-w-xs w-full" placeholder="Bio"></textarea>
+                    <textarea name="message" className="textarea textarea-ghost border-2 border-bordercolor bg-cardbg max-w-xs w-full" placeholder="Bio"></textarea>
                 </div>
                 <div className="flex flex-col gap-1 w-full max-w-xs">
-                    <button className="btn btn-neutral w-full max-w-xs">Neutral</button>
+                    <button type="submit" className="btn btn-neutral w-full max-w-xs">Neutral</button>
                 </div>
             </form>
         </div>
